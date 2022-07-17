58b8d245e92ee951c30f22359d4d01efdfada36a
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-07-17 19:58:14 +0300

    I will create new design
    html was written incorrectly, not easy compatible for mobile version... I will rewrite the full code for easy responsiveness

--- js/script.js
+++ js/script.js
@@ -2,22 +2,9 @@ const dropDownMenu = document.querySelectorAll('.dropDown');
 const dropDownArrow = document.querySelectorAll('.header__nav_heading img');
 const headerNav = document.querySelector('.header__nav_heading');
 
-// document.addEventListener('click', (e) => {
-//     // console.log(e.target)
-//     console.log(e.target !== headerNav)
 
-//     // if (e.target === headerNav) {
-//     //     dropDownArrow.forEach(element => {
-//     //         element.classList.remove('rotate');
-//     //         gsap.to('.header__nav-list', {
-//     //             height: 0,
-//     //             opacity: 0,
-//     //             duration: 0.3,
-//     //             display: 'none'
-//     //         })
-//     //     })
-//     // }
-// })
+
+
 
 dropDownMenu.forEach((element) => {
     // console.log(element.querySelector('.header__nav-list'))
@@ -29,6 +16,7 @@ dropDownMenu.forEach((element) => {
         let imgArrow = e.target.querySelector('img')
 
         if (imgArrow.classList.contains('rotate')) {
+
             imgArrow.classList.remove('rotate');
             gsap.to(dropDown, {
                 height: 0,
@@ -40,6 +28,7 @@ dropDownMenu.forEach((element) => {
         } else {
             dropDownArrow.forEach(element => {
                 element.classList.remove('rotate');
+
                 gsap.to('.header__nav-list', {
                     height: 0,
                     opacity: 0,
@@ -48,7 +37,12 @@ dropDownMenu.forEach((element) => {
                 })
             })
             imgArrow.classList.add('rotate');
-            gsap.to(dropDown, {
+
+            gsap.fromTo(dropDown, {
+                height: 0,
+                padding: 0,
+                display: 'none'
+            }, {
                 padding: '16px',
                 height: 'auto',
                 opacity: 1,
