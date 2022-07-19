51fc02f347a38253cc2ea17694183b61a554a005
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-07-20 00:38:48 +0300

    dropdown for mobile ready

--- js/script.js
+++ js/script.js
@@ -1,53 +1,97 @@
-const dropDownMenu = document.querySelectorAll('.dropDown');
-const dropDownArrow = document.querySelectorAll('.header__nav_heading img');
-const headerNav = document.querySelector('.header__nav_heading');
+const dropDownLink = document.querySelectorAll('.dropdown__inner');
+const dropDownArrow = document.querySelectorAll('.dropdown__link img');
+const dropDownContent = document.querySelectorAll('.dropdown__hiden-links');
+
+
+dropDownLink.forEach((element) => {
+
+    // console.log(element)
+    //     let dropDown = element.querySelector('.header__nav-list');
 
 
 
+    element.addEventListener('click', (e) => {
+        // console.log(element);
 
 
-dropDownMenu.forEach((element) => {
-    // console.log(element.querySelector('.header__nav-list'))
+        let arrow = e.target.querySelector('img');
+        let content = e.target.querySelector('.dropdown__hiden-links');
 
-    let dropDown = element.querySelector('.header__nav-list');
-    element.addEventListener('click', function (e) {
-        // console.log(e.target)
 
-        let imgArrow = e.target.querySelector('img')
-
-        if (imgArrow.classList.contains('rotate')) {
-
-            imgArrow.classList.remove('rotate');
-            gsap.to(dropDown, {
-                height: 0,
-                opacity: 0,
-                duration: 0.3,
-                display: 'none'
-
+        let closeContent = () => {
+            element.classList.remove('active')
+            gsap.to(arrow, {
+                rotate: '0deg'
             })
-        } else {
-            dropDownArrow.forEach(element => {
-                element.classList.remove('rotate');
-
-                gsap.to('.header__nav-list', {
-                    height: 0,
-                    opacity: 0,
-                    duration: 0.3,
-                    display: 'none'
-                })
-            })
-            imgArrow.classList.add('rotate');
-
-            gsap.fromTo(dropDown, {
+            gsap.to(content, {
                 height: 0,
-                padding: 0,
-                display: 'none'
-            }, {
-                padding: '16px',
-                height: 'auto',
-                opacity: 1,
-                display: 'inline-block'
+                opacity: 0
             })
         }
+        let openContent = () => {
+            element.classList.add('active')
+            gsap.to(arrow, {
+                rotate: '180deg'
+            })
+            gsap.to(content, {
+                height: 'auto',
+                opacity: 1
+            })
+        }
+
+        if (element.classList.contains('active')) {
+            closeContent()
+        } else {
+            dropDownLink.forEach((element) => {
+                console.log(element)
+                element.classList.remove('active')
+                gsap.to(dropDownArrow, {
+                    rotate: '0deg'
+                })
+                gsap.to(dropDownContent, {
+                    height: 0,
+                    opacity: 0
+                })
+            });
+            openContent()
+        }
     })
+
+    //         let imgArrow = e.target.querySelector('img')
+
+    //         if (imgArrow.classList.contains('rotate')) {
+
+    //             imgArrow.classList.remove('rotate');
+    //             gsap.to(dropDown, {
+    //                 height: 0,
+    //                 opacity: 0,
+    //                 duration: 0.3,
+    //                 display: 'none'
+
+    //             })
+    //         } else {
+    //             dropDownArrow.forEach(element => {
+    //                 element.classList.remove('rotate');
+
+    //                 gsap.to('.header__nav-list', {
+    //                     height: 0,
+    //                     opacity: 0,
+    //                     duration: 0.3,
+    //                     display: 'none'
+    //                 })
+    //             })
+    //             imgArrow.classList.add('rotate');
+
+    //             gsap.fromTo(dropDown, {
+    //                 height: 0,
+    //                 padding: 0,
+    //                 display: 'none'
+    //             }, {
+    //                 padding: '16px',
+    //                 height: 'auto',
+    //                 opacity: 1,
+    //                 display: 'inline-block'
+    //             })
+    //         }
+    //     })
 });
